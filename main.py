4c3a1d88d3c8c07acfc6fb11df7d67017d83bbd4
Thane Chaves <tmoonder@gmail.com>
2025-01-03 14:12:08 -0500

    Added pseudocode planning and created VSC file.

---
+++ main.py
@@ -0,0 +1,12 @@
+# read csv file with columns for name, math grade, science grade, and english grade
+
+# calculate average grade for each subject across all students
+
+# find the student(s) with highest overall grade
+# find average grade of each student
+# order students from highest to lowest grade
+# return highest graded student
+
+# find subject with highest grade
+
+# return all results in readable format
