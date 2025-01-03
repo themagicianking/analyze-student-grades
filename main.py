c1e83eb8055adbfbef123bafe44208762f042016
Thane Chaves <tmoonder@gmail.com>
2025-01-03 14:29:30 -0500

    Read CSV file and added values to relevant lists.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 # read csv file with columns for name, math grade, science grade, and english grade
+import csv
+
+names = []
+math = []
+science = []
+english = []
+
+with open('grades.csv') as csv_file:
+  grade_reader = csv.reader(csv_file)
+  i = 0
+  for row in grade_reader:
+    if i > 0:
+      names.append(row[0])
+      math.append(row[1])
+      science.append(row[2])
+      english.append(row[3])
+    i += 1
+  
+print(names, math, science, english)
 
 # calculate average grade for each subject across all students
 
